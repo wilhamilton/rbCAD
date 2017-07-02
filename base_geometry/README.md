ad9beac0d171b8e4e822edcc3ee56c26d0117675
Wil Hamilton <wil.hamilton@gmail.com>
2017-07-02 14:22:35 -0400

    updated base geometry module with documentation

---
+++ base_geometry/README.md
@@ -0,0 +1,9 @@
+#Base Geometry
+
+Base geometry classes used to aid in defining a part.
+
+The base classes include:
+
+* Coordinate System
+* Plane
+* Vertex
--- base_geometry/__init__.py
+++ base_geometry/__init__.py
@@ -1,3 +1,10 @@
+"""
+Initializer for Base Geometry Module.
+
+The base geometry module includes items that help define where
+and item is located in space.
+"""
+
 from base_geometry.CoordinateSystem import CoordinateSystem
 from base_geometry.Plane import Plane
 from base_geometry.Vertex import Vertex
